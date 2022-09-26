ef8b26afe98ce02e338487f0fe9e4c3ec8e01e0c
Adam De Rango <113118048+adamdera@users.noreply.github.com>
2022-09-26 09:33:58 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+	
+})
